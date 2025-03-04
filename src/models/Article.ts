4b7834090cbe90a64f6542837c2d2027db80dca5
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2025-03-04 01:58:11 +0000

    Created an Article model for MongoDB database

---
+++ src/models/Article.ts
@@ -0,0 +1,21 @@
+import mongoose, { Document, Schema } from 'mongoose'
+
+interface IArticle extends Document {
+    title: string
+    link: string
+    content?: string
+    contentSnippet?: string
+    source: string
+    pubDate: Date
+}
+
+const ArticleSchema: Schema = new Schema({
+    title: {type: String, required: true},
+    link: {type: String, required: true, unique: true},
+    content: String,
+    contentSnippet: String,
+    pubDate: {type: Date, required: true},
+    source: {type: String, required: true}
+}) 
+
+const Article = mongoose.model<IArticle>('Article', ArticleSchema);
