fe7947d9a96125017e20e630ecf4ed9b262bc2bf
Godfred777 <81054545+Godfred777@users.noreply.github.com>
2025-03-04 23:07:50 +0000

    refactor RSS feed parser to improve error handling and update feed URLs

--- src/services/parsers/rssParser.ts
+++ src/services/parsers/rssParser.ts
@@ -2,39 +2,45 @@ import Parser from 'rss-parser';
 import { Article } from '../../models/Article';
 
 
-
 const feedUrls = [
-    'https://rss.app/feeds/QWstunB5dW93zouY.xml',
-    'https://rss.app/feeds/82fYOxUR6iB8aOpS.xml',
+   // 'https://rss.app/feeds/QWstunB5dW93zouY.xml',
+  //  'https://rss.app/feeds/82fYOxUR6iB8aOpS.xml',
+    'http://feeds.bbci.co.uk/news/rss.xml',
+    'https://ir.thomsonreuters.com/rss/news-releases.xml?items=15',
+    'https://ir.thomsonreuters.com/rss/events.xml?items=15',
 ];
 
-
+// Fetch all feeds and save the articles to the database
 export async function fetchAllFeeds() {
-    const parser = new Parser();
-    const feedPromises = feedUrls.map(url => parser.parseURL(url));
-    const feeds = await Promise.all(feedPromises);
-  
-    feeds.forEach(feed => {
-      feed.items.forEach(async item => {
-        try {
-          await Article.findOneAndUpdate(
-            { link: item.link }, // search criteria
-            {   // update data
-                title: item.title,
-                link: item.link,
-                content: item.content,
-                pubDate: item.pubDate,
-                source: item.source,
-                image: item.image,
-            },
-            {
-                upsert: true, // create if doesn't exist
-                new: true, // return the updated document
-            }
-        );
-        } catch (error) {
-          console.error('Error saving article: ', error);
-        }
+    try {
+      const parser = new Parser();
+      const feedPromises = feedUrls.map(url => parser.parseURL(url));
+      const feeds = await Promise.all(feedPromises);
+    
+      feeds.forEach(feed => {
+        feed.items.forEach(async (item) => {
+          try {
+            await Article.findOneAndUpdate(
+              { link: item.link }, // search criteria
+              {   // update data
+                  title: item.title,
+                  link: item.link,
+                  content: item.content,
+                  pubDate: item.pubDate,
+                  source: item.source,
+                  image: item.image,
+              },
+              {
+                  upsert: true, // create if doesn't exist
+                  new: true, // return the updated document
+              }
+          );
+          } catch (error) {
+            console.error('Error saving article: ', error);
+          }
+        });
       });
-    });
+    } catch (error) {
+      console.error('Error fetching feeds: ', error);
+    }
 }
